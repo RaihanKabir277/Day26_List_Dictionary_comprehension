9ff07a5125d38d268d3b1394b15f3a504de4d109
RaihanKabir277 <raihank0198@gmail.com>
2025-07-20 18:01:57 +0600

    basic of comprehensive added here

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# -------list comprehension added here -----------
+
+numbers = [1,2,3]
+
+# new_list = []
+
+# for n in numbers:
+#     add_1 = n + 1
+
+# new_list.append(add_1)
+
+# ----------- alternettive to do this -----
+
+new_list = [n + 1 for n in numbers]
+
+
