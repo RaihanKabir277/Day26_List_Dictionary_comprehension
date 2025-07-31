19a199ac08695a67914600d52a0095a1994f01b7
RaihanKabir277 <raihank0198@gmail.com>
2025-07-31 07:48:03 +0600

    dictionary comprehension added here

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import random
 # -------list comprehension added here -----------
 
 numbers = [1,2,3]
@@ -31,12 +32,25 @@ range_list = [num * 2 for num in range(1,9)]
 
 names = ["raihan", "kabir", "rafsan", "robin"]
 
-short_names = [name for name in names if len(name) < 6]
+# short_names = [name for name in names if len(name) < 6]
 # print(short_names)
 
 # short_names = [name.upper() for name in names if len(name) < 6]
 
 # print(f"{[n.upper() for n in names if len(n) < 6]}")
 
+# -------------Dictionary comprehension added here-----------
 
+names = ["raihan", "kabir", "rafsan", "robin", "niloy","pathan"]
+
+# student_score = {
+#     "raihan" : 89,
+#     "rafsan" : 95
+# }
+
+student_score = {student : random.randint(1,100) for student in names}
+print(student_score)
+
+passed_students = {student : score for (student,score) in student_score.items() if score >= 60}
+print(passed_students)
 
